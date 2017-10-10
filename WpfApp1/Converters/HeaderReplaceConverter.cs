63bbb3aa562901ab7e3778b64819c6f46c32ace9
= <=>
2017-10-10 23:35:33 +0500

    add replaceing menu

---
+++ WpfApp1/Converters/HeaderReplaceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp1.Converters
+{
+    public class HeaderReplaceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = value;
+            return (double)value > 600;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
